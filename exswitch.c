9095fbdf7088307d16873d23ad5469d2f77adec1
Amarmandal <amarmandal2073@gmail.com>
2018-11-25 09:31:41 +0545

    Switch case example 1

---
+++ exswitch.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	char name[10];
+	printf("Enter the name");
+	scanf("%s", name);
+	switch (name){
+	case amar:{
+		printf("My name is amar");
+		break;	
+	}
+	default:{
+		printf("You are not amar");
+	}
+	}
+}
