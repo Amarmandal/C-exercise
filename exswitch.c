e4acdc65a45590049bd55db0a946b487d6a8cf4f
Amarmandal <amarmandal2073@gmail.com>
2018-12-13 21:12:41 +0545

    Another Change

--- exswitch.c
+++ exswitch.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 int main(){
 	char name[10];
-	printf("Enter the name");
+	printf("Enter the name: ");
 	scanf("%s", name);
 	switch (name){
-	case amar:{
-		printf("My name is amar");
+	case 'amar':{
+		printf("My name is amar\n");
 		break;	
 	}
 	default:{
-		printf("You are not amar");
+		printf("You are not amar\n");
 	}
 	}
 }
